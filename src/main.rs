28b99129bb0f77daecd4bfbb1b88ba8dc65b1053
asmetics <116861388+asmetics@users.noreply.github.com>
2024-11-13 12:23:21 -0600

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 use std::error::Error;
-
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
-    //ui.on_request_increase_value({
-      //  let ui_handle = ui.as_weak();
-        //move || {
-          //  let ui = ui_handle.unwrap();
-            //ui.set_counter(ui.get_counter() + 1);
-        //}
-    //});
+    // Home button callback
+    ui.on_home_clicked({
+        let ui_handle = ui.as_weak();
+        move || {
+            if let Some(ui) = ui_handle.upgrade() {
+                ui.set_is_home_page(true);
+                println!("Home page activated");
+            }
+        }
+    });
+
+    // Library button callback
+    ui.on_library_clicked({
+        let ui_handle = ui.as_weak();
+        move || {
+            if let Some(ui) = ui_handle.upgrade() {
+                ui.set_is_home_page(false);
+                println!("Library page activated");
+            }
+        }
+    });
 
     ui.run()?;
-
     Ok(())
 }
